066bbb2828c01e2a74c3d0b9ef78c279dae6c500
Tran Quoc Bao <bao.tq@sutrixsolutions.com>
2018-05-16 17:23:13 +0700

    Deleted some Font constant.

--- JustDraw/JustDraw/Shared/Constant/Constant.swift
+++ JustDraw/JustDraw/Shared/Constant/Constant.swift
@@ -11,10 +11,7 @@ import UIKit
 
 struct Constant {
     struct Font {
-        static let AvenirNextMedium13: UIFont = UIFont(name: "AvenirNext-Medium", size: 13) ?? UIFont.systemFont(ofSize: 13)
-        static let AvenirNextMedium14: UIFont = UIFont(name: "AvenirNext-Medium", size: 14) ?? UIFont.systemFont(ofSize: 14)
         static let AvenirNextMedium15: UIFont = UIFont(name: "AvenirNext-Medium", size: 15) ?? UIFont.systemFont(ofSize: 15)
-        static let AvenirNextBold18: UIFont = UIFont(name: "AvenirNext-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18)
     }
     struct CollectionView {
         struct Home {
